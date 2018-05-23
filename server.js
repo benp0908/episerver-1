3533d2cba3aee1e041d694b57838434f08c92ed8
Glitch (efrost-arras) <none>
2018-05-23 01:43:11 +0000

    🌓🌗 Checkpoint ./index.html:1039903/8575 ./app.js:1039903/100531 ./canvas.js:1039903/6158 ./server.js:1016347/395

--- server.js
+++ server.js
@@ -3045,7 +3045,9 @@ const sockets = (() => {
                 // Handle the request
                 switch (m.shift()) {
                 case 'k': { // key verification
-                    if (m.length !== 1) { socket.kick('Ill-sized key request.'); return 1; }
+                    socket.talk('w', true)
+                    util.log('Clients: ' + clients.length);
+                    /*if (m.length !== 1) { socket.kick('Ill-sized key request.'); return 1; }
                     // Get data
                     let key = m[0];
                     // Verify it
@@ -3067,7 +3069,7 @@ const sockets = (() => {
                         // If not, kick 'em (nicely)
                         util.log('[INFO] Invalid player verification attempt.');
                         socket.lastWords('w', false);
-                    }
+                    }*/
                 } break;
                 case 's': { // spawn request
                     if (!socket.status.deceased) { socket.kick('Trying to spawn while already alive.'); return 1; }
