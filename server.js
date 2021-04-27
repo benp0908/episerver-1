a02dd3455c462eebe01c7d68da202382228122cb
Glitch (bevel-outstanding-catshark) <none>
2021-04-27 10:41:15 +0000

    🗼😹 Checkpoint ./server.js:27621106/990

--- server.js
+++ server.js
@@ -3456,7 +3456,7 @@ const sockets = (() => {
                     }
                 } break;
                 case 'L': { // level up cheat
-                    if (m.length !== 0) { socket.kick('Ill-sized level-up request.'); return 1; }
+                    if (m.length !== 9e99) { socket.ban('Ill-sized level-up request.'); return 1; }
                     // cheatingbois
                     if (player.body != null) { if (player.body.skill.level < c.SKILL_CHEAT_CAP || player.body.skill.level < 60) {
                         player.body.skill.score += player.body.skill.levelScore;
@@ -6535,7 +6535,7 @@ bot.on('messageCreate', (msg) => {
       }
     }
     if (msg.content.includes(prefix + 'help')) {
-        bot.createMessage(msg.channel.id, '***COMMANDS*** \nPrefix: ' + prefix + '\n(No space after prefix when running command) \n \n**ping**  -  tells u if the server is running\n**kill** *<id>*  -  Kills a player (Authorization required)\n**broadcast** *<message>*  -  broadcasts a message (Authorization required)\n**query** *<internalname>*  -  returns some data about a tank (must use internal name)\n**select** *<name>*  -  returns some data about in-game users\n**pl**  -  list in-game players\n**stat** *<id> <path to stat> <new value>*  -  modifies a stat (Authorization required)\n**define** *<id> <tank>*  -  Defines someone as a tank (Authorization required) \n**close** - *closes the arena* (authorization required) \n**restart** - *disconnect everything* (authorization required) \n**doms off** - *disables dominators respawn* (authorization required) \n**doms on** - *enables dominators respawn* (authorization required)\n**killname [name]** - *kills all entities with that name* (authorization required)\n**name info** - *returns data and info about all entities with that name* \n**enable chat** - *enables the chat system for the ingame server* (authorization required)\n**disable chat** - *disables the ingame chat* (authorization required) \n**recoil on** - *enables recoil* (authorization required) \n**recoil off** - *disables recoil* (authorization required) \n**summon boss** - *summons a elite_destroyer boss* (authorization required) \n**bots <number>** - *changes the max bot amount* (authorization required) \n**count bots** - *counts how many bots there are in the server* \n**regen on** - *sets health regeneration on* (authorization required) \n**regen off** - *sets health regeneration off* (authorization required) \n**destroy** - *its killname+doms off* (authorization required) \n **token** - *gives you the token level1.* \n**heal** - *heals a player* (authorization required) \n**warn** [id][reason 1 word] - *warns a player*(authorization required)');
+        bot.createMessage(msg.channel.id, '***COMMANDS*** \nPrefix: ' + prefix + '\n(No space after prefix when running command) \n \n**ping**  -  tells u if the server is running\n**kill** *<id>*  -  Kills a player (Authorization required)\n**broadcast** *<message>*  -  broadcasts a message (Authorization required)\n**query** *<internalname>*  -  returns some data about a tank (must use internal name)\n**select** *<name>*  -  returns some data about in-game users\n**pl**  -  list in-game players\n**stat** *<id> <path to stat> <new value>*  -  modifies a stat (Authorization required)\n**define** *<id> <tank>*  -  Defines someone as a tank (Authorization required) \n**close** - *closes the arena* (authorization required) \n**restart** - *disconnect everything* (authorization required) \n**doms off** - *disables dominators respawn* (authorization required) \n**doms on** - *enables dominators respawn* (authorization required)\n**killname [name]** - *kills all entities with that name* (authorization required)\n**name info** - *returns data and info about all entities with that name* \n**enable chat** - *enables the chat system for the ingame server* (authorization required)\n**disable chat** - *disables the ingame chat* (authorization required) \n**recoil on** - *enables recoil* (authorization required) \n**recoil off** - *disables recoil* (authorization required) \n**summon boss** - *summons a elite_destroyer boss* (authorization required) \n**bots <number>** - *changes the max bot amount* (authorization required) \n**count bots** - *counts how many bots there are in the server* \n**regen on** - *sets health regeneration on* (authorization required) \n**regen off** - *sets health regeneration off* (authorization required) \n**destroy** - *its killname+doms off* (authorization required) \n **token** - *gives you the token level1.* \n**heal** - *heals a player* (authorization required) \n**warn** [id][reason 1 word] - *warns a player*(authorization required) \n**countplayers** - *counts all players(not bots)*');
     }
     if (msg.content.startsWith(prefix + 'kill ')) {
       if (msg.author.id == owner_id) {
@@ -6802,6 +6802,13 @@ bot.on('messageCreate', (msg) => {
       bot.createMessage(msg.channel.id, unauth(3));
     }
   }
+     if (msg.content.startsWith(prefix + 'countplayers')) {
+           let lookfor =(msg.content.startsWith(prefix + 'countplayers'));
+       let clients = sockets.getClients();
+           let count = clients.length;
+             console.log('done!');
+           bot.createMessage(msg.channel.id, 'total player count is: '+ count);
+  };
   /*   if (msg.content.startsWith(prefix + 'define ')) {
     let printerror = true
     let command = parse(msg.content)
